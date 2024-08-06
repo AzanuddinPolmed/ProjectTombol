c46a59c80aa8e7992d72b9739cb5b26d121a4b9f
Azanuddin <azdin.bpc@gmail.com>
2024-08-06 15:55:14 +0700

    Manambahakn Menu dan Membuat 3 Activity

--- app/src/main/java/com/example/dtshello/MainActivity.java
+++ app/src/main/java/com/example/dtshello/MainActivity.java
@@ -3,6 +3,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,10 +20,18 @@ public class MainActivity extends AppCompatActivity {
         bproses.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                bproses.setBackgroundColor(Color.RED);
+                bproses.setBackgroundColor(Color.CYAN);
                 Toast.makeText(MainActivity.this, "Tombol Jadi Merah", Toast.LENGTH_SHORT).show();
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.optionmenu,menu);
+        return true;
     }
 }
